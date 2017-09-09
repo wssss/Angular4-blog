c131830bf347a56ef482cfb20c06cd9a741d95fe
weis <578402809@qq.com>
2017-09-09 10:30:44 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+const appRoutes: Routes = [
+    {
+        path: 'posts',
+        component:'PostsComponent'
+    },
+    {
+        path: '',
+        redirectTo: 'posts',
+    }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PostsComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
